b83adbc92fe412b30312cb32c80a533e18052c4f
xiebanglin <2419507265@qq.com>
2020-12-24 21:50:55 +0800

    完成Sleuth

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/controller/SendMessageController.java
@@ -0,0 +1,20 @@
+package com.atguigu.controller;
+
+import com.atguigu.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+
+}
